43de4ecb8b261c58e974ef5054fc304624472973
Filipe <a21905158@alunos.ulht.pt>
2022-05-03 12:29:33 +0100

    Added users and groups.

--- Session_Phases/admin.py
+++ Session_Phases/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from Session_Phases.models import Session, Phase
+from Session_Phases.models import *
 
 # Register your models here.
 
 admin.site.register(Session)
 admin.site.register(Phase)
+
+admin.site.register(Cog_Group)
+
+admin.site.register(Facilitator)
+admin.site.register(Participant)
--- Session_Phases/models.py
+++ Session_Phases/models.py
@@ -1,10 +1,11 @@
+from unicodedata import name
 from django.db import models
 from django.utils.translation import gettext_lazy
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
-
+#Session related section
 class Regime(models.TextChoices):
         ONLINE = 'O', gettext_lazy('Online')
         PRESENCIAL = 'P', gettext_lazy('Presencial')
@@ -33,3 +34,36 @@ class Phase(models.Model):
 
     def __str__(self):
         return f"Phase nº: {self.number} From session: {self.session.number}| Topic: {self.topic} | Duration: {self.duration}"
+
+#Group related section
+class Cog_Group(models.Model):
+
+    def __str__(self):
+        return f"Group id: {self.id}"
+
+#User related section
+class User(models.Model):
+
+    name = models.CharField(max_length= 26)
+    email = models.EmailField()
+    class Meta:
+        abstract = True 
+    def __str__(self):
+        return f"User name: {self.name} | User id: {self.id}"
+
+    
+
+class Facilitator(User):
+
+    grupo_cog = models.ManyToManyField(Cog_Group, blank = True, related_name ="facilitator")
+
+    def __str__(self):
+        return f"Facilitator name: {self.name} | Facilitator id: {self.id}"
+
+    
+class Participant(User):
+
+    grupo_cog = models.ManyToManyField(Cog_Group, blank= True, related_name="participant")
+
+    def __str__(self):
+        return f"Participant name: {self.name} | Participant id: {self.id}"
